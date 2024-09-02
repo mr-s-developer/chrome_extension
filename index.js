c0e770ee0353049088c3bbe78bf2db031d8ab760
Sergey <sergey.sokolov.ssa@gmail.com>
2024-09-02 11:48:07 +0500

    ADDED: Create the unordered list and Render the <li> elements with innerHTML and Create the render function

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+let listItems = ""
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  renderLeads()
 })
+
+function renderLeads(){
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+      listItems += "<li>" + myLeads[i] + "</li>"
+  }
+  ulEl.innerHTML = listItems  
+}
